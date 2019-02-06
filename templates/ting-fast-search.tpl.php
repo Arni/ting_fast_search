a87605fa35581ab882c14f9dee056024224c463c
DQ26525 <DQ26525@LT4750.Randers.dk>
2019-02-06 15:51:56 +0100

    Basic structure

---
+++ templates/ting-fast-search.tpl.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<div class="ting-object view-mode-teaser clearfix">
+   <div class="ting-object view-mode-search-result imagestyle-ding-list-medium list-item-style clearfix">
+      <div class="inner">
+        <?php print $cover; ?>
+         <div id="ting-object-ting-object-search-result-group-ting-right-col-search" class="ting-object-right group-ting-right-col-search field-group-div field-group-format group-ting-right-col-search">
+            <div class="field field-name-ting-title field-type-ting-title field-label-hidden">
+               <span class="search-result--heading-type js-toggle-info-container">Ebog:</span>
+               <h2><?php print $title; ?></h2>
+            </div>
+            <div class="field field-name-ting-author field-type-ting-author field-label-hidden"><?php print $creators; ?></div>
+            <div id="ting-object-ting-object-search-result-group-info" class="info-container group-info field-group-div field-group-format group-info">
+               <div class="field field-name-ting-abstract field-type-ting-abstract field-label-hidden">
+                  <div class="field-items">
+                     <div class="field-item even"></div>
+                  </div>
+               </div>
+               <div class="availability search-result--availability">
+                  <strong>Lånemuligheder</strong>
+                  <p class="js-online">Online:
+                     <a id="availability-870970-basis53743765-ebog" class="js-search-result--availability-link availability available" href="/ting/object/870970-basis:53743765">Ebog</a>
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
