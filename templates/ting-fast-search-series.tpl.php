e1a53ce6d7357c45588c0d93798c1f60cd7c1dae
DQ26525 <DQ26525@LT4750.Randers.dk>
2019-02-07 13:26:38 +0100

    Basic structure

---
+++ templates/ting-fast-search-series.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<div class="field field-name-ting-series field-type-ting-series field-label-above">
+  <div class="field-label">Serie</div>
+  <div class="field-items">
+    <div class="field-item even"><?php print $series; ?></div>
+  </div>
+</div>
--- templates/ting-fast-search.tpl.php
+++ templates/ting-fast-search.tpl.php
@@ -8,20 +8,20 @@
         <?php print $cover; ?>
          <div id="ting-object-ting-object-search-result-group-ting-right-col-search" class="ting-object-right group-ting-right-col-search field-group-div field-group-format group-ting-right-col-search">
             <div class="field field-name-ting-title field-type-ting-title field-label-hidden">
-               <span class="search-result--heading-type js-toggle-info-container">Ebog:</span>
+               <span class="search-result--heading-type js-toggle-info-container"><?php print $maintype; ?></span>
                <h2><?php print $title; ?></h2>
             </div>
             <div class="field field-name-ting-author field-type-ting-author field-label-hidden"><?php print $creators; ?></div>
             <div id="ting-object-ting-object-search-result-group-info" class="info-container group-info field-group-div field-group-format group-info">
                <div class="field field-name-ting-abstract field-type-ting-abstract field-label-hidden">
                   <div class="field-items">
-                     <div class="field-item even"></div>
+                     <div class="field-item even"><?php print $abstract; ?></div>
                   </div>
                </div>
+              <?php print $series; ?>
                <div class="availability search-result--availability">
                   <strong>Lånemuligheder</strong>
-                  <p class="js-online">Online:
-                     <a id="availability-870970-basis53743765-ebog" class="js-search-result--availability-link availability available" href="/ting/object/870970-basis:53743765">Ebog</a>
+                  <p class="js-online"><?php print implode($types); ?>
                   </p>
                </div>
             </div>
